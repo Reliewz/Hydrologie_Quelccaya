41785bb014ba7f1e63c34cadb76a56e5be2ba1ff
Reliewz <zwiessler.kai@web.de>
2025-12-02 14:55:25 +0100

    adding a generic date function with parse_date_time

--- RScripts/01_import/load_and_standardize.R
+++ RScripts/01_import/load_and_standardize.R
@@ -1,48 +1,15 @@
 #======================================================================
 # Scriptname: load_and_standardize.R
 # Goal(s): 
-  # Documentation of steps taken in Power Quiery
   # Change Date format to POSIXct
-  # Preperation of Analysis of temporal consistency -> Note that the "time_diff" column will be established for each data set but             analyszed is the respective QC_ scripts.
+  # Preperation of Analysis of temporal consistency for each dataset the in depth- analysis will be taken place in the respective QC_ scripts.
 # Author: Kai Albert Zwießler
 # Date: 2025.11.14
-# Input Dataset: Hydrological_data/piezometer_data/PZ_merged/PZ_merged/All_PZ_merged.xlsx
+# Input Dataset: 
 # Outputs: 
-  # figures/temperature_timeseries.png	#
-# Units:
-  # Abs.pres <- kPa
-  # Temp <- °C
-# Sensor information:
-  # PZ01  S/N: 21826509
-  # PZ02  S/N: 21826502
-  # PZ03  S/N: 21826497
-  # PZ04  S/N: 21826519
-  # PZ05  S/N: 21826512
-  # PZ06  S/N: 21826504
-  # PZ07  S/N: 21826505
-  # PZ08  S/N: 21826596
-  # PZ09  S/N: 21826594
-  # PZ010 S/N: 21826516
-  # PZ011 S/N: 21826500
-  # PZ12  S/N: 21826503
+  # 
 
-#======================================================================
-##Documentation of Power Quiery steps##
-# All Piezometer data has been loaded from one folder.
-# First column removed from every data set with example file
-# Correct columns assigned as headlines
-# Changed column names to universal terms across all data sets
-# Adding customized column to convert the date from US to format: YYYY-MM-DD hh:mm:ss
-# = DateTime.FromText([#"Date Time, GMT-5:00"], "en-US")
-# = DateTime.ToText([Date_Standardized], "yyyy-MM-dd HH:mm:ss")
-# Changing order of column & removing old Date column
-# Changing type to decimal with location information "English (USA)"
-# Assigning a Source.Name to every line
-# Extracting Piezometer_ID and WLS_ID from source.name
-# All Piezometers and WLS data are merged together in one Excel File and are now ready to analyze them in RStudio.
 
-# Further steps documented in the R Script below.
-#======================================================================
 
 
 # ========== Load Library ==========
@@ -53,18 +20,21 @@ library(readxl)     # Für Excel-Import
 library(stringr)    # Added to extract Piezometer ID part of tidyr
 
 
-#Sources
-
-
+# Import Data Sources
+# Met Station Qori_Kalis "D:\\RProjekte\\Hydrologie_Quelccaya\\Datenquellen\\STATION_QORIKALIS\\Meteorological_data\\processed\\QORIKALIS_merged.xlsx"
+# Piezometer "D:/RProjekte/Hydrologie_Quelccaya/Datenquellen/Hydrological_data/piezometer_data/PZ_merged/PZ_merged/All_PZ_merged.xlsx"
 
 # ========== CONFIGURATION ==========
 #Dataframe
-input_file <- "D:/RProjekte/Hydrologie_Quelccaya/Datenquellen/Hydrological_data/waterlevel_data/WLS_merged.xlsx"
+input_file <- "D:\\RProjekte\\Hydrologie_Quelccaya\\Datenquellen\\STATION_QORIKALIS\\Meteorological_data\\processed\\QORIKALIS_merged.xlsx"
+#alternative paths: 
+# WLS: D:/RProjekte/Hydrologie_Quelccaya/Datenquellen/Hydrological_data/waterlevel_data/WLS_merged.xlsx
 #Parameters
 sheet_name <- "Rinput"
 date_column <- "Date"        # Column name for timestamp
 id_column <- "ID"         # Column for identification
 output_column <- "time_diff" # Column for calculated output
+timediff_column <- "time_diff" # Column for further analysis in the field of temporal consistency 
 # ===================================
 message("Column names have been assigned beforehand in Power Quiery===")
 # ========== STEP 1: LOAD DATA ==========
@@ -86,9 +56,18 @@ cat("\n=== STEP 2: Standardize date format to ISO 8601 ===\n")
 cat("Original date format (first entry):", data_raw[[date_column]][1], "\n")
 cat("Original date class:", class(data_raw[[date_column]]), "\n")
 
-# Convert US format (MM.DD.YY hh:mm:ss AM/PM) to ISO 8601
+# Convert different date formats like US/EU (..) to ISO 8601 YYYY.MM.DD hh:mm:ss
 data_raw <- data_raw %>%
-  mutate(Date = mdy_hms(!!sym(date_column), tz = "Etc/GMT+5"))
+  mutate(
+    Date = parse_date_time(
+      x = !!sym(date_column),
+      orders = c(
+        "ymd HMS", "dmy HMS", "mdy HMS", 
+        "mdy HMS p", "ymd HM", "dmy HM"
+      ),
+      tz = "Etc/GMT+5"
+    )
+  )
 
 # ========== Validation after transformation ==========
 cat("Validate date convertion...")
@@ -115,9 +94,10 @@ if (any(is.na(data_raw[[date_column]]))) {
 } else {
   # All dates parsed successfully
   cat("✓ Date conversion successful. All dates parsed correctly. The Date column contains no NA values")
+  message("✓ Date conversion successful. All dates parsed correctly. The Date column contains no NA values")
 }
   
-#Assigning other columns in their seperate scripts
+# Assigning other columns in their separate scripts
 message("Inbetween verification === Are all columns assigned the correct type?===")
 print(str(data_raw))
 
@@ -136,7 +116,4 @@ if (id_column %in% names(data_raw)) {
 # Show results
 cat("First 3 rows after sorting:\n")
 print(head(data_raw, 3))
-
-# ========== STEP 4: Remove columns that have no information content ==========
-cat("\n=== STEP 4: Removing columns without information content ===\n")
 
